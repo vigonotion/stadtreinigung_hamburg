13ecff405b3825a1bd42cbcd020d3cd50e604eb8
vigonotion <tom@sutomaji.net>
2022-08-08 10:38:56 +0200

    Fix garbage collection url

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/vigonotion/stadtreinigung_hamburg'
 EMAIL = 'tom@sutomaji.net'
 AUTHOR = 'Tom Schneider'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.3.0'
+VERSION = '0.3.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
--- stadtreinigung_hamburg/StadtreinigungHamburg.py
+++ stadtreinigung_hamburg/StadtreinigungHamburg.py
@@ -61,7 +61,7 @@ class StadtreinigungHamburg:
         
 
         # now fetch the actual garbage pickups
-        r = session.get(F"https://www.stadtreinigung.hamburg/abfuhrkalender/?tx_srh_pickups%5Bstreet%5D={asId}&tx_srh_pickups%5Bhousenumber%5D={hnId}")
+        r = session.get(F"https://www.stadtreinigung.hamburg/abfuhrkalender/?tx_srh_pickups%5Bstreet%5D={asId}&tx_srh_pickups%5Bhousenumber%5D={hnId}&tx_srh_pickups%5BisAllowedOwner%5D=1")
         if(r.status_code!=200): raise Exception(F"Fetching step2 failed: Status code was {r.status_code}!")
         
         collections = []
